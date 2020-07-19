3e17cf8b9b3a6f7df53673842979f864a8857185
zaknicholsdev <zaknicholsdev1993@gmail.com>
2020-07-18 21:48:10 -0400

    Updates

--- precache-manifest.bd575736d737bfcc7b7df3053aab3414.js
+++ precache-manifest.bd575736d737bfcc7b7df3053aab3414.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1f026d5c1c2713adf042bd7dc0d09fe",
+    "revision": "a6e9802828405074daa9571c3fae52b9",
     "url": "/Moovy/index.html"
   },
   {
-    "revision": "e5d8640b8d549c87fdb1",
+    "revision": "614d91e49c56897345aa",
     "url": "/Moovy/static/css/main.7437e101.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Moovy/static/js/2.cc2630e0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e5d8640b8d549c87fdb1",
-    "url": "/Moovy/static/js/main.92b25e93.chunk.js"
+    "revision": "614d91e49c56897345aa",
+    "url": "/Moovy/static/js/main.9a132e85.chunk.js"
   },
   {
     "revision": "e73d1e5468ed3585d9d1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Moovy/precache-manifest.869767402880eafaebc2764dbeafcaff.js"
+  "/Moovy/precache-manifest.bd575736d737bfcc7b7df3053aab3414.js"
 );
 
 self.addEventListener('message', (event) => {
