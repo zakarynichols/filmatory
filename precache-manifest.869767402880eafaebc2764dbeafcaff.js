2a39c6cded851115bf5bb297ec4f2b87a40639fa
zaknicholsdev <zaknicholsdev1993@gmail.com>
2020-07-18 21:30:29 -0400

    Updates

---
+++ precache-manifest.869767402880eafaebc2764dbeafcaff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f026d5c1c2713adf042bd7dc0d09fe",
+    "url": "/Moovy/index.html"
+  },
+  {
+    "revision": "e5d8640b8d549c87fdb1",
+    "url": "/Moovy/static/css/main.7437e101.chunk.css"
+  },
+  {
+    "revision": "654c1f40b8033c6c5144",
+    "url": "/Moovy/static/js/2.cc2630e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Moovy/static/js/2.cc2630e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d8640b8d549c87fdb1",
+    "url": "/Moovy/static/js/main.92b25e93.chunk.js"
+  },
+  {
+    "revision": "e73d1e5468ed3585d9d1",
+    "url": "/Moovy/static/js/runtime-main.83e308ad.js"
+  }
+]);
